38f71ae8449a8566c5f2c724d5961a6fa44acc97
Clos2023Clos <138270956+Clos2023Clos@users.noreply.github.com>
2026-09-23 15:53:46 -0700

    Fix selected hero image MIME type

--- lib/hero/index.ts
+++ lib/hero/index.ts
@@ -5,5 +5,5 @@ import c3 from './chunk03';
 import c4 from './chunk04';
 import c5 from './chunk05';
 
-const heroSrc = `data:image/avif;base64,${c0}${c1}${c2}${c3}${c4}${c5}`;
+const heroSrc = `data:image/jpeg;base64,${c0}${c1}${c2}${c3}${c4}${c5}`;
 export default heroSrc;
