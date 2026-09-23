c26092052cd45e5649267a409cad0b6dfdbe3e01
Clos2023Clos <138270956+Clos2023Clos@users.noreply.github.com>
2026-09-23 15:26:29 -0700

    Assemble selected hero image

---
+++ lib/hero/index.ts
@@ -0,0 +1,9 @@
+import c0 from './chunk00';
+import c1 from './chunk01';
+import c2 from './chunk02';
+import c3 from './chunk03';
+import c4 from './chunk04';
+import c5 from './chunk05';
+
+const heroSrc = `data:image/avif;base64,${c0}${c1}${c2}${c3}${c4}${c5}`;
+export default heroSrc;
