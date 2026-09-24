9fa2062ea76b8600c3610e157dd7936eb47dee69
Clos2023Clos <138270956+Clos2023Clos@users.noreply.github.com>
2026-09-23 17:09:43 -0700

    Assemble complete live hero image

---
+++ lib/hero-live/index.ts
@@ -0,0 +1,15 @@
+import c0 from './chunk00';
+import c1 from './chunk01';
+import c2 from './chunk02';
+import c3 from './chunk03';
+import c4 from './chunk04';
+import c5 from './chunk05';
+import c6 from './chunk06';
+import c7 from './chunk07';
+import c8 from './chunk08';
+import c9 from './chunk09';
+import c10 from './chunk10';
+import c11 from './chunk11';
+
+const heroSrc = `data:image/webp;base64,${c0}${c1}${c2}${c3}${c4}${c5}${c6}${c7}${c8}${c9}${c10}${c11}`;
+export default heroSrc;
