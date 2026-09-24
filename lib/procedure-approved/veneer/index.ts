ede7e60176d24486edb5702addde3d86145fc5fd
Clos2023Clos <138270956+Clos2023Clos@users.noreply.github.com>
2026-09-24 06:55:35 -0700

    Assemble approved veneer image

---
+++ lib/procedure-approved/veneer/index.ts
@@ -0,0 +1,7 @@
+import c0 from './chunk00';
+import c1 from './chunk01';
+import c2 from './chunk02';
+import c3 from './chunk03';
+
+const src = 'data:image/webp;base64,' + c0 + c1 + c2 + c3;
+export default src;
