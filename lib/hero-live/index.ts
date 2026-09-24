2638d6bc80b9893722caeb4930824eda5fc9b119
Clos2023Clos <138270956+Clos2023Clos@users.noreply.github.com>
2026-09-23 17:19:07 -0700

    Assemble complete split hero chunk

--- lib/hero-live/index.ts
+++ lib/hero-live/index.ts
@@ -5,11 +5,12 @@ import c3 from './chunk03';
 import c4 from './chunk04';
 import c5 from './chunk05';
 import c6 from './chunk06';
-import c7 from './chunk07';
+import c7a from './chunk07a';
+import c7b from './chunk07b';
 import c8 from './chunk08';
 import c9 from './chunk09';
 import c10 from './chunk10';
 import c11 from './chunk11';
 
-const heroSrc = `data:image/webp;base64,${c0}${c1}${c2}${c3}${c4}${c5}${c6}${c7}${c8}${c9}${c10}${c11}`;
+const heroSrc = `data:image/webp;base64,${c0}${c1}${c2}${c3}${c4}${c5}${c6}${c7a}${c7b}${c8}${c9}${c10}${c11}`;
 export default heroSrc;
