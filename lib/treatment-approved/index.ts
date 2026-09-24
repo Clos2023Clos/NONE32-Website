e61ae9dc41bb070450ea3eed3614c1f401177947
Clos2023Clos <138270956+Clos2023Clos@users.noreply.github.com>
2026-09-24 07:04:00 -0700

    Assemble exact approved treatment artwork

---
+++ lib/treatment-approved/index.ts
@@ -0,0 +1,9 @@
+import c0 from './chunk00';
+import c1 from './chunk01';
+import c2 from './chunk02';
+import c3 from './chunk03';
+import c4 from './chunk04';
+
+const approvedTreatmentComposite = 'data:image/webp;base64,' + c0 + c1 + c2 + c3 + c4;
+
+export default approvedTreatmentComposite;
